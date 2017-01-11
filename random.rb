570cdb2df8a8301a54732ac9ca3cf761f8a48e85
Salomon Beza <beza.salomon@gmail.com>
2017-01-11 12:11:29 +0200

    Just made changes to my files. I added the second line on both files. Completing the exercises

--- random.rb
+++ random.rb
@@ -1,1 +1,3 @@
 puts "This is my second folder to push on GitHub"
+
+puts "Now I am trying to add another line of code, and commit again"
--- sample.rb
+++ sample.rb
@@ -1,1 +1,3 @@
 puts "another to be pushed within my folder"
+
+puts "Try to add a line to this file as well"
